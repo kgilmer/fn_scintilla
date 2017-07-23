dfd295a2a8a58129d50c5f5b577c87dd346601a5
Ken Gilmer <kgilmer@gmail.com>
2017-07-23 13:38:52 -0700

    Initial commit.

---
+++ src/main/java/com/abk/fn_scintilla/Consumer.java
@@ -0,0 +1,8 @@
+package com.abk.fn_scintilla;
+
+/**
+ * Created from https://stackoverflow.com/questions/22905562/functional-interfaces-of-java-8-in-java-7
+ */
+public interface Consumer<T> {
+    void accept(T t);
+}
---
+++ src/main/java/com/abk/fn_scintilla/Function.java
@@ -0,0 +1,8 @@
+package com.abk.fn_scintilla;
+
+/**
+ * Created from https://stackoverflow.com/questions/22905562/functional-interfaces-of-java-8-in-java-7
+ */
+public interface Function<T, R> {
+    R apply(T t);
+}
